05dae6d6ad09024508f0f4a8610c6a6cace9bf01
Guillaume Lauroy <gui.lauroy@gmail.com>
2024-04-29 15:45:49 +0200

    update inscription db

--- litrevu/authentification/views.py
+++ litrevu/authentification/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.shortcuts import render
 
 
 def authentification(request):
@@ -12,3 +14,23 @@ def inscription(request):
 
 def connexion(request):
     return render(request, "authentification/connexion.html")
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password1 = request.POST["password1"]
+        password2 = request.POST["password2"]
+
+        if password1 == password2:
+            if User.objects.filter(username=username).exists():
+                return HttpResponse("Cet utilisateur existe déjà")
+            else:
+                user = User.objects.create_user(username=username, password=password1)
+                user.save()
+                login(request, user)
+                return redirect("flux")
+        else:
+            return HttpResponse("Les mots de passe ne correspondent pas")
+    else:
+        return render(request, "authentification/inscription.html")
