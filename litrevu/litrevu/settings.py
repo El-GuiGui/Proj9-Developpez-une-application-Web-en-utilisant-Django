13dea46e359a2564500b8eeb55fe30ef6e7c8ece
Guillaume Lauroy <gui.lauroy@gmail.com>
2024-05-31 19:11:39 +0200

    add com

--- litrevu/litrevu/settings.py
+++ litrevu/litrevu/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_extensions",
-    "debug_toolbar",
     "authentification",
     "tickets",
     "subscriptions",
@@ -54,7 +53,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    "litrevu.middleware.LoginRequiredMiddleware",
 ]
 
 ROOT_URLCONF = "litrevu.urls"
